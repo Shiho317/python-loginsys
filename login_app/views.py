910a77e8a98e5b070678fdd3082b66fdbaebb7af
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-24 11:25:08 -0800

    remove folder

---
+++ login_app/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+
+def home(request):
+    return render(request, "authentication/index.html")
+
+def signup(request):
+    if request.method == "POST":
+      username = request.POST['username']
+      firstname = request.POST['firstname']
+      lastname = request.POST['lastname']
+      email = request.POST['email']
+      password = request.POST['password']
+      comfirm = request.POST['comfirm']
+
+      myuser = User.objects.create_user(username, email, password)
+      myuser.first_name = firstname
+      myuser.last_name = lastname
+
+      myuser.save()
+
+      messages.success(request, "Your Account has been successfully created.")
+
+      return redirect('signin')
+      
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+# def signout(request):
+#     return render(request, "login_app/signup.html")
